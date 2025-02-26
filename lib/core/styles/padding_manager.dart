43c60f233e37f32317c1508ee1e141b758a43a15
Your Name <your_email@example.com>
2025-02-26 22:02:18 +0200

    Refactor PaddingManager for responsive padding calculations and simplify edge insets management

--- lib/core/styles/padding_manager.dart
+++ lib/core/styles/padding_manager.dart
@@ -1,52 +1,41 @@
 import 'package:flutter/material.dart';
 
 class PaddingManager {
-  static EdgeInsets main(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return EdgeInsets.symmetric(
-      horizontal: size.width * 0.05,
-      // vertical: size.height * 0.03,
-    );
+  static double _responsiveSize(BuildContext context, double value) {
+    double baseSize =
+        (MediaQuery.of(context).size.width +
+            MediaQuery.of(context).size.height) /
+        2;
+    return (value / 1000) * baseSize;
   }
 
-  static EdgeInsets paddingHorizontal04Vertical015(BuildContext context) {
-    return EdgeInsets.symmetric(
-      horizontal: MediaQuery.of(context).size.width * 0.04,
-      vertical: MediaQuery.of(context).size.height * 0.015,
-    );
-  }
-
-  static EdgeInsets paddingHorizontal10(BuildContext context) {
-    return EdgeInsets.symmetric(
-      horizontal: MediaQuery.of(context).size.width * 0.1,
-    );
-  }
-
-  static EdgeInsets vertical05(BuildContext context) {
-    return EdgeInsets.symmetric(
-      vertical: MediaQuery.of(context).size.height * 0.05,
-    );
-  }
-
-  static EdgeInsets vertical03(BuildContext context) {
-    return EdgeInsets.symmetric(
-      vertical: MediaQuery.of(context).size.height * 0.03,
-    );
-  }
-
-  static EdgeInsets custom({
+  static EdgeInsets symmetric({
     required BuildContext context,
-    double left = 0,
-    double top = 0,
-    double right = 0,
-    double bottom = 0,
+    double horizontal = 16,
+    double vertical = 16,
   }) {
-    final size = MediaQuery.of(context).size;
+    return EdgeInsets.symmetric(
+      horizontal: _responsiveSize(context, horizontal),
+      vertical: _responsiveSize(context, vertical),
+    );
+  }
+
+  static EdgeInsets all({required BuildContext context, double value = 16}) {
+    return EdgeInsets.all(_responsiveSize(context, value));
+  }
+
+  static EdgeInsets only({
+    required BuildContext context,
+    double left = 0.0,
+    double right = 0.0,
+    double top = 0.0,
+    double bottom = 0.0,
+  }) {
     return EdgeInsets.only(
-      left: left > 0 ? left : size.width * 0.04,
-      top: top > 0 ? top : size.height * 0.02,
-      right: right > 0 ? right : size.width * 0.04,
-      bottom: bottom > 0 ? bottom : size.height * 0.02,
+      left: _responsiveSize(context, left),
+      right: _responsiveSize(context, right),
+      top: _responsiveSize(context, top),
+      bottom: _responsiveSize(context, bottom),
     );
   }
 }

3a5a7b8580e51c90a95b4e9c4fa7b1602ef8815b
Your Name <your_email@example.com>
2025-02-26 21:00:39 +0200

    Update color definitions for improved theming and accessibility

--- lib/core/styles/color_manager.dart
+++ lib/core/styles/color_manager.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const primary = Color(0xff105D5E);
-  static const tertiary = Color(0xff293E33);
-  static const background = Color(0xffE1E7E0);
-  static const secondary = Color(0xffE85613);
+  static const primary = Color(0xff1B5E37);
+  static const secondary = Color(0xffF4A91F);
+  static const back = Colors.black;
+  static const important = Color(0xffEB5757);
   static const gray = Color(0xff767F7D);
-  static const success = Color(0xff22A901);
-  static const firebrick = Color(0xffB01717);
+  static const white = Colors.white;
 }

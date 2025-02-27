f3b2f0ab2ed9363bb69837969b8b8421801f1116
Your Name <your_email@example.com>
2025-02-27 20:25:14 +0200

    Add darkPrimary color constant to AppColor for enhanced theming options

--- lib/core/styles/color_manager.dart
+++ lib/core/styles/color_manager.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const primary = Color(0xff1B5E37);
   static const lightPrimary = Color(0xff2D9F5D);
+  static const darkPrimary = Color(0xff1B5E37);
   static const secondary = Color(0xffF4A91F);
   static const back = Colors.black;
   static const important = Color(0xffEB5757);

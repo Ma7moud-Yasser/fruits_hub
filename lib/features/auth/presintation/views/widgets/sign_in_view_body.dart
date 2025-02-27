6a7be972e050ec8895809b99e578bd821dd8781b
Your Name <your_email@example.com>
2025-02-27 20:54:04 +0200

    Refactor SignInViewBody to use CustomScrollView and add SocialAuthButton for improved UI and functionality

--- lib/features/auth/presintation/views/widgets/sign_in_view_body.dart
+++ lib/features/auth/presintation/views/widgets/sign_in_view_body.dart
@@ -1,83 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:fruits_hub/core/components/custom_button.dart';
 import 'package:fruits_hub/core/components/custom_text_form.dart';
 import 'package:fruits_hub/core/helper/app_strings.dart';
 import 'package:fruits_hub/core/styles/assets_manager.dart';
-import 'package:fruits_hub/core/styles/border_radius_manager.dart';
 import 'package:fruits_hub/core/styles/color_manager.dart';
 import 'package:fruits_hub/core/styles/padding_manager.dart';
 import 'package:fruits_hub/core/styles/sized_box_manager.dart';
 import 'package:fruits_hub/core/styles/styles_manager.dart';
 import 'package:fruits_hub/features/auth/components/have_or_not_have_account_widget.dart';
 import 'package:fruits_hub/features/auth/components/or_divider.dart';
+import 'package:fruits_hub/features/auth/presintation/views/widgets/social_auth_button.dart';
 
 class SignInViewBody extends StatelessWidget {
   const SignInViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: PaddingManager.symmetric(context: context, vertical: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            CustomTextFormField(
-              hintText: AppStrings.email,
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBoxManager.height(context, 16),
-            CustomTextFormField(
-              hintText: AppStrings.password,
-              keyboardType: TextInputType.visiblePassword,
-              suffixIconWidget: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.visibility, color: AppColor.stoneLight),
+    return CustomScrollView(
+      physics: ClampingScrollPhysics(), // منع التمرير المرن على iOS
+      slivers: [
+        SliverPadding(
+          padding: PaddingManager.symmetric(context: context, vertical: 24),
+          sliver: SliverList(
+            delegate: SliverChildListDelegate([
+              CustomTextFormField(
+                hintText: AppStrings.email,
+                keyboardType: TextInputType.emailAddress,
               ),
-            ),
-            SizedBoxManager.height(context, 16),
-            Text(
-              AppStrings.forgotPassword,
-              style: StyleManager.textStyle14(
-                context,
-                FontWeight.w600,
-              ).copyWith(color: AppColor.lightPrimary),
-            ),
-            SizedBoxManager.height(context, 33),
-            CustomButton(onPressed: () {}, text: AppStrings.signIn),
-            SizedBoxManager.height(context, 33),
-            HaveOrNotHaveAccount(isSignIn: false, onTap: () {}),
-            SizedBoxManager.height(context, 33),
-            const ORDivider(),
-            SizedBox(
-              // height: 56,
-              height: SizedBoxManager.responsiveSize(context, 100),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: AppColor.silverSky,
-
-                  shape: ContinuousRectangleBorder(
-                    borderRadius: BorderRadiusManager.medium(context),
-                  ),
-                  side: BorderSide(color: AppColor.darkSilverSky, width: 1),
-                ),
-                onPressed: () {},
-                child: ListTile(
-                  visualDensity: VisualDensity(
-                    vertical: VisualDensity.minimumDensity,
-                  ),
-                  leading: SvgPicture.asset(IconsAssets.googleIcon, width: 24),
-                  title: Text(
-                    AppStrings.signInWithGoogle,
-                    style: StyleManager.textStyle16(context, FontWeight.w600),
-                  ),
+              SizedBoxManager.height(context, 16),
+              CustomTextFormField(
+                hintText: AppStrings.password,
+                keyboardType: TextInputType.visiblePassword,
+                suffixIconWidget: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.visibility, color: AppColor.stoneLight),
                 ),
               ),
-            ),
-          ],
+              SizedBoxManager.height(context, 16),
+              Text(
+                textAlign: TextAlign.left,
+                AppStrings.forgotPassword,
+                style: StyleManager.textStyle14(
+                  context,
+                  FontWeight.w600,
+                ).copyWith(color: AppColor.lightPrimary),
+              ),
+              SizedBoxManager.height(context, 33),
+              CustomButton(onPressed: () {}, text: AppStrings.signIn),
+              SizedBoxManager.height(context, 33),
+              HaveOrNotHaveAccount(isSignIn: false, onTap: () {}),
+              SizedBoxManager.height(context, 33),
+              const ORDivider(),
+              SocialAuthButton(
+                title: AppStrings.signInWithGoogle,
+                iconPath: IconsAssets.googleIcon,
+                onPressed: () {},
+              ),
+              SocialAuthButton(
+                title: AppStrings.signInWithApple,
+                iconPath: IconsAssets.appleIcon,
+                onPressed: () {},
+              ),
+              SocialAuthButton(
+                title: AppStrings.signInWithFacebook,
+                iconPath: IconsAssets.facebookIcon,
+                onPressed: () {},
+              ),
+            ]),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
---
+++ lib/features/auth/presintation/views/widgets/social_auth_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub/core/styles/border_radius_manager.dart';
+import 'package:fruits_hub/core/styles/color_manager.dart';
+import 'package:fruits_hub/core/styles/padding_manager.dart';
+import 'package:fruits_hub/core/styles/styles_manager.dart';
+
+class SocialAuthButton extends StatelessWidget {
+  const SocialAuthButton({
+    super.key,
+    required this.title,
+    required this.iconPath,
+    this.onPressed,
+  });
+  final String title;
+  final String iconPath;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: PaddingManager.only(context: context, top: 16),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape: ContinuousRectangleBorder(
+            borderRadius: BorderRadiusManager.medium(context),
+            side: BorderSide(color: AppColor.darkSilverSky, width: 1),
+          ),
+        ),
+        onPressed: () {},
+        child: ListTile(
+          visualDensity: VisualDensity(vertical: VisualDensity.minimumDensity),
+          leading: SvgPicture.asset(iconPath, width: 30),
+          title: Text(
+            textAlign: TextAlign.center,
+            title,
+            style: StyleManager.textStyle16(context, FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+}

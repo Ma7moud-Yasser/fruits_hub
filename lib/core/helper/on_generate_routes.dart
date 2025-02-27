039231429f82c413445b06b8f6d248a4950c4109
Your Name <your_email@example.com>
2025-02-27 02:41:48 +0200

    Add SignInScreen and update route generation for navigation

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/auth/presintation/views/sign_in_screen.dart';
 import 'package:fruits_hub/features/on_boarding/presintaion/views/on_boarding_screen.dart';
 import 'package:fruits_hub/features/splash/presintation/views/splash_screen.dart';
 
 class Routes {
   static const String splash = '/splashScreen';
   static const String onBoardingScreen = '/onBoardingScreen';
+  static const String signInScreen = '/signInScreen';
 
   static const String home = '/';
 }
@@ -16,6 +18,8 @@ class OnGeneraRouteGenerator {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case Routes.signInScreen:
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
       default:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
     }

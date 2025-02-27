535b5d90a3ff95d036e11903123812429ae2b03e
Your Name <your_email@example.com>
2025-02-27 20:24:48 +0200

    Rename private method _responsiveSize to public method responsiveSize for improved accessibility

--- lib/core/styles/sized_box_manager.dart
+++ lib/core/styles/sized_box_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SizedBoxManager {
-  static double _responsiveSize(BuildContext context, double value) {
+  static double responsiveSize(BuildContext context, double value) {
     double baseSize =
         (MediaQuery.of(context).size.width +
             MediaQuery.of(context).size.height) /
@@ -10,15 +10,15 @@ class SizedBoxManager {
   }
 
   static SizedBox height(BuildContext context, double value) {
-    return SizedBox(height: _responsiveSize(context, value));
+    return SizedBox(height: responsiveSize(context, value));
   }
 
   static SizedBox width(BuildContext context, double value) {
-    return SizedBox(width: _responsiveSize(context, value));
+    return SizedBox(width: responsiveSize(context, value));
   }
 
   static SizedBox square(BuildContext context, double value) {
-    double size = _responsiveSize(context, value);
+    double size = responsiveSize(context, value);
     return SizedBox(width: size, height: size);
   }
 }

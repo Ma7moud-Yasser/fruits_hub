56f61b677be21d035944b1000cebdda5145797bd
Your Name <your_email@example.com>
2025-02-27 19:04:28 +0200

    Add new color constants for light primary, dark silver sky, and stone light

--- lib/core/styles/color_manager.dart
+++ lib/core/styles/color_manager.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const primary = Color(0xff1B5E37);
+  static const lightPrimary = Color(0xff2D9F5D);
   static const secondary = Color(0xffF4A91F);
   static const back = Colors.black;
   static const important = Color(0xffEB5757);
   static const gray = Color(0xff767F7D);
-  static const silverSky = Color(0xffE6E9EA);
+  static const silverSky = Color(0xffF9FAFA);
+  static const darkSilverSky = Color(0xffE6E9EA);
+  static const stoneLight = Color(0xff949D9E);
   static const white = Colors.white;
 }

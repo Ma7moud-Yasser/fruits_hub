af10c0fcc91c470cdd57fa36bf81f9b22735e972
Your Name <your_email@example.com>
2025-02-27 02:58:49 +0200

    Add SharedPrefs singleton for managing shared preferences and initialize in main

---
+++ lib/core/services/shared_preferences_singleton.dart
@@ -0,0 +1,48 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefs {
+  static final SharedPrefs _instance = SharedPrefs._internal();
+  static SharedPreferences? _prefs;
+
+  factory SharedPrefs() {
+    return _instance;
+  }
+
+  SharedPrefs._internal();
+
+  static Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  static Future<void> setString(String key, String value) async {
+    await _prefs?.setString(key, value);
+  }
+
+  static String? getString(String key) {
+    return _prefs?.getString(key);
+  }
+
+  static Future<void> setInt(String key, int value) async {
+    await _prefs?.setInt(key, value);
+  }
+
+  static int? getInt(String key) {
+    return _prefs?.getInt(key);
+  }
+
+  static Future<void> setBool(String key, bool value) async {
+    await _prefs?.setBool(key, value);
+  }
+
+  static bool? getBool(String key) {
+    return _prefs?.getBool(key);
+  }
+
+  static Future<void> remove(String key) async {
+    await _prefs?.remove(key);
+  }
+
+  static Future<void> clear() async {
+    await _prefs?.clear();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/helper/on_generate_routes.dart';
+import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/features/splash/presintation/views/splash_screen.dart';
 
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPrefs.init();
   runApp(const FruitHub());
 }
 

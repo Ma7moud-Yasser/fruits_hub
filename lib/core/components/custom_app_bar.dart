13783b2e29bbea7d9110be97da6a4d0277c52c2e
Your Name <your_email@example.com>
2025-02-28 16:09:00 +0200

    Update customAppBar to conditionally hide back button on sign-in screen

--- lib/core/components/custom_app_bar.dart
+++ lib/core/components/custom_app_bar.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/helper/app_strings.dart';
 import 'package:fruits_hub/core/styles/styles_manager.dart';
 
 AppBar customAppBar(BuildContext context, {required String title}) {
   return AppBar(
     automaticallyImplyLeading: false,
     centerTitle: true,
-    leading: IconButton(
-      onPressed: () {
-        Navigator.pop(context);
-      },
-      icon: Icon(Icons.arrow_back_ios_new),
-    ),
+    leading:
+        title == AppStrings.signIn
+            ? null
+            : IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios_new),
+            ),
     title: Text(
       title,
       style: StyleManager.textStyle20(context, FontWeight.bold),

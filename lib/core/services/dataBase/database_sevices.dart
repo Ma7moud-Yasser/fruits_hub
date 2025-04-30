5a6caa54cee83d612251413c13c960c3f8a41b56
Your Name <your_email@example.com>
2025-04-30 17:24:21 +0300

    refactor: add missing line breaks for improved readability in database_services.dart

--- lib/core/services/dataBase/database_sevices.dart
+++ lib/core/services/dataBase/database_sevices.dart
@@ -1,6 +1,10 @@
+import 'package:fruits_hub/core/entities/user_entity.dart';
+
 abstract class DataBaseServices {
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
   });
+
+  Future<UserEntity> getUserData({required String path, required String uid});
 }
